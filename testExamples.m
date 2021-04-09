bdb7bcaca9b7e20135908973dd40128f734a14d6
Luke Tokheim <luketokheim@gmail.com>
2021-04-09 11:32:28 -0700

    Implement local function wrappers for all examples as unit tests.
    Added README example code as test cases.

--- testExamples.m
+++ testExamples.m
@@ -34,7 +34,7 @@
 %
 
 function tests = testExamples
-  f = {@examplePlotAll, @examplePlotPositionTrail};
+  f = localfunctions();
   if exist('functiontests')
     % Matlab
     tests = functiontests(f);
@@ -44,11 +44,72 @@ function tests = testExamples
     display('--- Running testExamples ---');
     tic;
 
-    for i=1:size(f, 2)
-      feval(f{1, i});
+    for i=1:size(f, 1)
+      figure;
+      feval(f{i, 1});
     end
   
     toc
     display('--- Done testExamples ---');
   end
+end
+
+function testPlotAll(testCase)
+  examplePlotAll();
+end
+
+function testPlotPositionTrail(testCase)
+  examplePlotPositionTrail();
+end
+
+function testReadmeBasic(testCase)
+  % Find your most recent take, read its data stream, and plot all of the data
+  % in the take at once.
+  plot(shadow.takeread());
+end
+
+function testReadmeRandom(testCase)
+  % Read back the data stream and the header so we show the time axis for one
+  % randomly selected channel.
+  [A, header] = shadow.takeread();
+
+  % Number of samples, one per row
+  n = size(A, 1);
+
+  % Create a time axis in seconds
+  x = [0:n-1] * header.h;
+
+  % Select a channel, one column
+  y = A(:, randi(n));
+
+  % Plot one channel over time
+  plot(x, y);
+end
+
+function testReadmeRange(testCase)
+  % Pick the gyroscope channel from the Hips node and create a nicer plot.
+  [A, header] = shadow.takeread();
+
+  % Node key 5 is the Hips in Shadow full body take
+  key = 5;
+  name = shadow.header.name();
+
+  % Find the gyroscope measurement channel
+  g_range = shadow.header.range(header, key, name.g);
+  if isempty(g_range)
+    error('no gyroscope measurement for the Hips node');
+  end
+
+  % Number of samples, one per row
+  n = size(A, 1);
+
+  % Create a time axis in seconds
+  x = [0:n-1] * header.h;
+
+  % Plot gyroscope data, x-y-z order in deg/s
+  plot(x, A(:, g_range));
+  title('Gyroscope measurements for Hips');
+  xlabel('Time (s)');
+  ylabel('Angular rate (deg/s)');
+  legend('Hips.gx', 'Hips.gy', 'Hips.gz');
 end
